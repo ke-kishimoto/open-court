ab4c17af6ef7b4259179b1b7cb958ceedf3d4d16
kishimoto keisuke <k.kishimoto@di-million@co.jp>
2020-01-02 22:43:23 +0900

    first commit

---
+++ detail.php
@@ -0,0 +1,30 @@
+<p>日付：</p>
+<p>時間：</p>
+<p>場所：</p>
+
+<p>【参加予定  0人】</p>
+<p>社会人：女性 0人、男性 0人
+<p>大学・専門：女性 0人、男性 0人</p>
+<p>高校生：女性 0人、男性 0人</p>
+
+<br>
+<hr
+<p>【応募フォーム】</p>
+<form action="" method="post">
+    <p>
+        職種：
+        社会人<input type="radio" name="occupation" value="1">
+        大学・専門<input type="radio" name="occupation" value="2">
+        高校生<input type="radio" name="occupation" value="3">
+    </p>
+    <p>
+        性別：
+        男性<input type="radio" name="sex" value="1">
+        女性<input type="radio" name="sex" value="2">
+    </p>
+    <p>
+        名前：
+        <input type="text" name="name">
+    </p>
+    <button type="submit">参加</button>
+</form>
