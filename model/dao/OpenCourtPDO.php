ea118aaa553d5a2b1b344f1fad00dfa72ebde7a6
kishimoto keisuke <k.kishimoto@di-million.co.jp>
2020-02-24 10:36:45 +0900

    DB接続修正

--- model/dao/OpenCourtPDO.php
+++ model/dao/OpenCourtPDO.php
@@ -1,17 +1,17 @@
 <?php
 class OpenCourtPDO extends PDO{
 
-    // const DATABASE_URL = 'postgres://pxxsmjadcejqnv:8a68cb4f3e311442d688439ea25d780f7e580d0263da70afe65181f20c1a705b@ec2-174-129-255-15.compute-1.amazonaws.com:5432/dd2fk9n2pggjn7';
-    const DSN = 'pgsql:dbname=open_court host=localhost port=54321';
-    const USER = 'axiz';
-    const PASSWORD = 'axiz';
+    const DATABASE_URL = 'postgres://pxxsmjadcejqnv:8a68cb4f3e311442d688439ea25d780f7e580d0263da70afe65181f20c1a705b@ec2-174-129-255-15.compute-1.amazonaws.com:5432/dd2fk9n2pggjn7';
+    // const DSN = 'pgsql:dbname=open_court host=localhost port=54321';
+    // const USER = 'axiz';
+    // const PASSWORD = 'axiz';
     
     public function __construct()
     {
 
-        // $url = parse_url(getenv('DATABASE_URL'));
-        // $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-        parent::__construct(OpenCourtPDO::DSN, OpenCourtPDO::USER, OpenCourtPDO::PASSWORD);
-        // parent::__construct($dsn, $url['user'], $url['pass']);
+        $url = parse_url(getenv('DATABASE_URL'));
+        $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+        // parent::__construct(OpenCourtPDO::DSN, OpenCourtPDO::USER, OpenCourtPDO::PASSWORD);
+        parent::__construct($dsn, $url['user'], $url['pass']);
     }
 }
