72b5249d7070d57b4d8810c5daec00c208399493
kishimoto keisuke <k.kishimoto@di-million.co.jp>
2020-08-01 23:07:23 +0900

    日時のフォーマット修正

--- controller/EventList.php
+++ controller/EventList.php
@@ -7,7 +7,15 @@ use dao\GameInfoDao;
 
 $gameInfoPDO = new GameInfoDao();
 $gameInfoList = $gameInfoPDO->getGameInfoListByDate($_POST['date']);
-
+$week = [
+    '日', //0
+    '月', //1
+    '火', //2
+    '水', //3
+    '木', //4
+    '金', //5
+    '土', //6
+  ];
 foreach ($gameInfoList as $gameInfo) {
     // echo 'aaaa';
     // echo $_POST['date'];
@@ -20,7 +28,9 @@ foreach ($gameInfoList as $gameInfo) {
         echo '<a href="EventDetail.php?id=' . $gameInfo['id'] . '">';
     }
     echo $gameInfo['title'] . '<br>';
-    echo '日時：' . $gameInfo['game_date'] . $gameInfo['start_time'] . '～' . $gameInfo['end_time'] . '<br>';
+    // echo '日時：' . $gameInfo['game_date'] . $gameInfo['start_time'] . '～' . $gameInfo['end_time'] . '<br>';
+    echo '日時：' . date('n月d日（', strtotime($gameInfo['game_date'])) . $week[date('w', strtotime($gameInfo['game_date']))] . '）';
+    echo $gameInfo['start_time'] . '～' . $gameInfo['end_time'] . '<br>';
     echo '場所：' . $gameInfo['place'];
     echo '</a>';
     echo '</li>';
