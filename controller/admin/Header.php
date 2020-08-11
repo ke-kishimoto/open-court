1393172226d9dfa3dffde8811a767007354622cd
Kondo <kondo@sabira.jp>
2020-08-11 16:31:08 +0900

    管理者画面にログイン機能を実装 (登録画面は用意していないので、adminFlg='1'のデータを用意してそれでログインしてください)

--- controller/admin/Header.php
+++ controller/admin/Header.php
@@ -3,6 +3,9 @@ require_once('../../model/dao/ConfigDao.php');
 use dao\ConfigDao;
 $configDao = new ConfigDao();
 $config = $configDao->getConfig(1);
+if(!isset($_SESSION)){
+    session_start();
+}
 if (!isset($_SESSION['system_title'])) {
     $_SESSION['system_title'] = $config['system_title'];
 }
@@ -13,4 +16,9 @@ if ($config['bg_color'] == 'white') {
 } else {
     $bgColor = 'bg-color-white';
 }
+//セッションに'user'が無ければログイン画面へ
+if (isset($_SESSION['user']) == null) {
+    header('Location: signIn.php');
+} 
+
 ?>
