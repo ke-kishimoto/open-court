fb91cd1888213f8e6e373cbbf6b565e96859aae1
Kondo <kondo@sabira.jp>
2020-08-11 16:32:24 +0900

    Revert "Revert "管理者画面にログイン機能を実装 (登録画面は用意していないので、adminFlg='1'のデータを用意してそれでログインしてください)""
    This reverts commit 58fc60a13374f0742685e56f61630c89565231d0.

--- view/common/header.php
+++ view/common/header.php
@@ -4,10 +4,10 @@
     </div>
     
     <div class="nav">
-        <span id="user_name" class=""><?php echo $userName ?>さん</span>
-        <a class="btn btn-sm btn-outline-dark <?php echo htmlspecialchars($noLoginClass) ?>" href="./SignUp.php" role="button">新規登録</a>
+        <span id="user_name" class="user-name"><?php echo $userName ?>さん</span>
+        <a class="btn btn-sm btn-outline-dark <?php echo htmlspecialchars($noLoginClass) ?>" href="./SignUp.php" role="button" style="margin-right:5px;">新規登録</a>
         <a class="btn btn-sm btn-outline-dark <?php echo htmlspecialchars($noLoginClass) ?>" href="./SignIn.php" role="button">ログイン</a>
-        <a class="btn btn-sm btn-outline-dark <?php echo htmlspecialchars($loginClass) ?>" href="./SignUp.php?id=<?php echo $id ?>" role="button">アカウント情報</a>
+        <a class="btn btn-sm btn-outline-dark <?php echo htmlspecialchars($loginClass) ?>" href="./SignUp.php?id=<?php echo $id ?>" role="button" style="margin-right:5px;">アカウント情報</a>
         <a class="btn btn-sm btn-outline-dark <?php echo htmlspecialchars($loginClass) ?>" href="./LogoutController.php" role="button">ログアウト</a>
     </div>
     
