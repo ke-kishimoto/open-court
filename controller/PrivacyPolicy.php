ea2506eed00afe651b8fa4f76b239be958dd773e
kishimoto-keisuke <nebinosuk@gmail.com>
2020-09-06 14:53:03 +0900

    プライバシーポリシーの追加

---
+++ controller/PrivacyPolicy.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+require_once('./header.php');
+
+$title = 'プライバシーポリシー';
+include('../view/common/head.php');
+include('../view/common/header.php');
+include('../view/privacyPolicy.php');
+include('../view/common/footer.php');
--- view/common/footer.php
+++ view/common/footer.php
@@ -1,3 +1,9 @@
+<hr>
+<footer class="<?php echo $_SESSION['bgColor'] ?> ">
+    <!-- <a href="#">当サイトについて</a> -->
+    <a href="PrivacyPolicy.php">プライバシーポリシー</a>
+    <a href="Inquiry.php">お問い合わせ</a>
+</footer>
 <script src="../js/common.js">
 </script>
 </body>
