c0124383c51b86b767796968a66840d9c2ec7b8e
kishimoto-keisuke <nebinosuk@gmail.com>
2020-10-26 00:31:42 +0900

    タイムスタンプの対応（仕掛かり）

--- SQL/database.sql
+++ SQL/database.sql
@@ -19,7 +19,13 @@ create table game_info (
     , place varchar(100)
     , limit_number int
     , detail varchar(1000)
+    , register_date date
+    , update_date date
 );
+
+-- 追加用
+-- alter table game_info add column register_date date;
+-- alter table game_info add column update_date date;
 
 -- イベントのテンプレ
 -- drop table event_template; 
@@ -31,7 +37,15 @@ create table event_template (
     , place varchar(100)
     , limit_number int
     , detail varchar(1000)
+    , register_date date
+    , update_date date
 );
+
+-- 追加用
+-- alter table event_template add column register_date date;
+-- alter table event_template add column update_date date;
+
+
 
 -- 参加者
 -- drop table participant;
@@ -43,10 +57,16 @@ create table participant (
     , name varchar(50)   -- 参加者名
     , email varchar(50)  -- メール
     , waiting_flg int -- キャンセル待ちフラグ 0：通常、1：キャンセル待ち
-    , remark varchar(200)
+    , remark varchar(200)  -- 備考
+    , register_date date   -- 登録日時
+    , update_date date     -- 更新日時
 );
 -- インデックス
 create index participant_idx_game on participant (game_id); 
+
+-- 追加用
+-- alter table participant add column register_date date;
+-- alter table participant add column update_date date;
 
 -- 同伴者
 -- drop table companion;
@@ -56,9 +76,15 @@ create table companion (
     , occupation int   -- 職種  1：社会、2：大学生、3：高校生
     , sex int -- 性別  1：男、2：女
     , name varchar(50)   -- 参加者名
+    , register_date date
+    , update_date date
 );
 -- インデックス
 create index companion_idx_participant on companion (participant_id);
+
+-- 追加用
+-- alter table companion add column register_date date;
+-- alter table companion add column update_date date;
 
 -- 設定
 -- 後々はユーザー単位にしたいな
@@ -70,7 +96,6 @@ create table config(
     , bg_color varchar(30)  -- 背景色、何個か選べるようにする
     , logo_img_path varchar(200)
 );
-
 
 -- 追加用
 -- alter table config add column bg_color varchar(30);
@@ -86,8 +111,16 @@ create table users(
   , password varchar(200) -- ハッシュ化して保存
   , occupation int   -- 職種  1：社会、2：大学生、3：高校生
   , sex int -- 性別  1：男、2：女
-  , remark varchar(200)
+  , remark varchar(200)  -- 備考
+  , register_date timestamp   -- 登録日時
+  , update_date timestamp     -- 更新日時
 );
+
+-- 追加用
+-- alter table users add column register_date timestamp;
+-- alter table users add column update_date timestamp;
+-- alter table users modify column register_date timestamp;
+-- alter table users modify column update_date timestamp;
 
 -- ユーザーに付随する同伴者の初期値
 -- drop table default_companion;
@@ -97,7 +130,13 @@ create table default_companion(
     , occupation int   -- 職種  1：社会、2：大学生、3：高校生
     , sex int -- 性別  1：男、2：女
     , name varchar(50)   -- 参加者名
+    , register_date date
+    , update_date date
 );
+
+-- 追加用
+-- alter table default_companion add column register_date date;
+-- alter table default_companion add column update_date date;
 
 -- 問い合わせ
 -- drop table inquiry; 
@@ -108,7 +147,7 @@ create table inquiry(
     , email varchar(50)
     , content varchar(2000)
     , status_flg int
-    , register_date date
-    , update_date date
+    , register_date timestamp
+    , update_date timestamp
 );
 
