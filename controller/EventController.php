ca08146b1750ceab38d895e3eb82e8522d1015e9
Kondo <kondo@sabira.jp>
2020-12-25 16:35:33 +0900

    「13月」と無い月が表示されるバグを修正。&monthはStrgin型だったので===ではなく==比較に変更

--- controller/EventController.php
+++ controller/EventController.php
@@ -17,12 +17,12 @@ class EventController extends BaseController
         $year = isset($_GET['year']) ? intval($_GET['year']) : date('Y') ;
         // $month = date('n');
         $month = isset($_GET['month']) ? intval($_GET['month']) : date('n');
-        $lastmonth = $month === 1 ? 12 : $month - 1;
-        $nextmonth = $month === 12 ? 1 : $month + 1;
+        $lastmonth = $month == 1 ? 12 : $month - 1;
+        $nextmonth = $month == 12 ? 1 : $month + 1;
         // $lastmonth = date('n',strtotime('-1 month'));
         // $nextmonth = date('n',strtotime('+1 month'));
-        $pre_year = $month === 1 ? $year - 1 : $year;
-        $next_year = $month === 12 ? $year + 1 : $year;
+        $pre_year = $month == 1 ? $year - 1 : $year;
+        $next_year = $month == 12 ? $year + 1 : $year;
 
         // 月末日を取得
         $last_day = date('j', mktime(0, 0, 0, $month + 1, 0, $year));
