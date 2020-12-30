849239d4f9319ce847501c25a8d9b3226a95837c
kishimoto keisuke <k.kishimoto@di-million.co.jp>
2020-12-31 00:58:09 +0900

    サーバー移行に伴いトップページを修正。

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php 
-header('Location: ./controller/index.php');
-?>
+<!DOCTYPE html>
+<head>
+    <title>サーバー移行に関するお知らせ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body class="container">
+<h1>サーバー移行に関するお知らせ</h1>
+<p>日頃からイベント予約システムをご活用いただき誠にありがとうございます。</p>
+<p>この度、システムの今後のことを考慮し、サーバーを移行することと致しました。</p>
+<p>移行に伴いURLが変更となるため、お手数ですが下記URLを新しくブックマークしていただくようお願い申し上げます。</p>
+<p><a href="https://opencourt.eventmanc.com/">https://opencourt.eventmanc.com/</a></p>
+
+<br>
+<p>※当サイトのURLは2021年1月末を持ってアクセス不可にする予定です。</p>
+</body>
