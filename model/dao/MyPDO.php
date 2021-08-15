80fe3e28328a817f4898792777334a0b882df785
kishimoto-keisuke <nebinosuk@gmail.com>
2021-08-15 12:02:23 +0900

    .gitignoreコミット

--- model/dao/MyPDO.php
+++ model/dao/MyPDO.php
@@ -18,7 +18,8 @@ class MyPDO extends PDO{
             self::DSN, self::USER, self::PASSWORD
         , 
     [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 
     }
